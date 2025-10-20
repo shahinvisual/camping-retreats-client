786bf4cc716301a7e1018023309a575187cef11b
shahinvisual <shahinalam1084@gmail.com>
2025-10-20 18:36:05 +0600

    navbar and home page compleate

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const navLink = <>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/updateProfile'>Update Profile</Link></li>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        </div>
+                        <ul
+                            tabIndex="-1"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            {navLink}
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">Camping Retreats</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navLink}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <a className="btn">Login</a>
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
